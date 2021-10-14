d4f8457726b587db1807e6e528caa710b25b6f68
RtxgmcStore <92449620+RtxgmcStore@users.noreply.github.com>
2021-10-14 16:21:00 +0800

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,8 +1,8 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6283144570377', 'RakuChan', m)
-stc = conn.sendContact(m.chat, '6285718366706', 'ALAN BOTZ', m)
-  conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Developer A L A N  B O T *', 'status@broadcast')
+  // F this.sendContact(m.chat, '6285397907992', 'Rtxgmc', m)
+stc = conn.sendContact(m.chat, '62852403679546', 'RtxgmcStore', m)
+  conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Developer ZeroBot *', 'status@broadcast')
 }
 handler.command = ['owner']
 
